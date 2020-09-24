414505af8375eaa6a80b5abe2ffa3300aa5306b0
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-09-24 14:17:59 +0300

    playing

--- src/index.js
+++ src/index.js
@@ -1,45 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
-  }
-]
+import axios from 'axios'
 
-const App = (props) => {
-  const { notes } = props
-
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note =>
-          <li key={note.id}>
-            {note.content}
-          </li>
-        )}
-      </ul>
-    </div>
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  ReactDOM.render(
+    <App notes={notes} />,
+    document.getElementById('root')
   )
-}
+})
+
+const promise = axios.get('http://localhost:3001/notes')
+console.log(promise)
 
 ReactDOM.render(
-  <App notes={notes} />,
+  <App />,
   document.getElementById('root')
 )
